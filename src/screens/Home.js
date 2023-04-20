4c562bd42260c3ea09b4ceb22440eabd2016cb5e
Nuss93 <nasreenrzk@gmail.com>
2023-04-21 00:40:20 +0800

    UPDATE HOME LAYOUT

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,23 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import BottomTabNav from '../components/BottomTabNav'
 import { Styles } from '../stylesheet';
 import StepTracker from '../components/StepTracker';
 import Shake from '../components/Shake';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkLocation, getCity } from '../features/userSlice';
+import { getCurrentWeather } from '../features/weatherSlice';
 
 
 const styles = StyleSheet.create(Styles)
 export default function Home() {
   const {location, city} = useSelector((state) => state.user)
+  const {current,loading} = useSelector((state) => state.weather)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // dispatch(getCurrentWeather(location))
+  }, [])
   
   return (
     <SafeAreaView style={{flex:1, backgroundColor: '#000'}}>
@@ -21,8 +28,18 @@ export default function Home() {
             {location ? <Text>{city}</Text> : null}
           </View>
 
-          {/* <StepTracker /> */}
-          <Shake />
+          <View style={{...styles.padding30, flexDirection:'row'}}>
+            <View style={{flex:1, ...styles.homeCard}}>
+              <Text>Set your goals</Text>
+            </View>
+            <View style={{flex:1, ...styles.homeCard}}>
+              <Text>Today's weather</Text>
+            </View>
+          </View>
+
+          <View style={{backgroundColor:'#3e424b',borderTopColor:'#3e424b', borderTopWidth:20, borderTopLeftRadius:25, borderTopRightRadius:25, alignContent:'stretch', bottom:0, position:'absolute', width:'100%', paddingBottom:30}}>
+            <Shake />
+          </View>
         </View>
 
         <BottomTabNav />
--- src/stylesheet.js
+++ src/stylesheet.js
@@ -2,22 +2,28 @@ export const Styles = {
     // ====== Generic styles ====== //
     safeAreaView : { flex: 1, justifyContent: 'center', alignItems: 'center' },
     padding50: { padding:50 },
+    padding30: { padding:30 },
     padding20: { padding:20 },
+    homeCard: {
+        backgroundColor: '#d9dddc',
+        padding:20,
+        marginRight:10
+    },
     weatherCard_header: {
-        backgroundColor: '#735F32',
+        backgroundColor: '#3e424b',
         padding: 20,
         borderRadius: 5,
         marginBottom: 10,
     },
     weatherCard_hourly: {
-        backgroundColor:'#282A3A',
+        backgroundColor:'#544c4a',
         width: 100,
         padding:20,
         marginRight:10,
         borderRadius: 10,
     },
     weatherCard_daily: {
-        // backgroundColor:'#282A3A',
+        // backgroundColor:'#544c4a',
         width: '100%',
         padding:10,
         marginBottom:10,
